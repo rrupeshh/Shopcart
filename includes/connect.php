18ec3e1a22dd645f9b7975a8258220743f1d6df4
rrupeshh <rupesh.poudel07@gmail.com>
2018-08-06 18:16:30 +0545

    Added connect

---
+++ includes/connect.php
@@ -0,0 +1,4 @@
+<?php 
+	$con = mysqli_connect('localhost','root','','shopcart') or die('Error connecting database!');
+	//$con = mysqli_connect('localhost','root','imatolymph123','shopcart') or die('Error connecting database!');
+?>
