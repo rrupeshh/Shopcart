62651ecbbd0a90d876d5019f9caf1b6b97c633d4
rrupeshh <rupesh.poudel07@gmail.com>
2018-08-14 10:31:10 +0545

    Added navbar

---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<div id = "navbar">
+	<ul>
+		<li><a href = "index.php">All Categories</a></li>
+		<li><a href = "mobile.php">Mobile &amp; Tablets</a></li>
+		<li><a href = "electronics.php">Electronics</a></li>
+		<li><a href = "computer.php">Computer &amp; Gaming</a></li>
+		<li><a href = "home.php">Home &amp; Kitchen</a></li>
+		<li><a href = "men.php">Men's Fashion</a></li>
+		<li><a href = "women.php">Women's Fashion</a></li>
+	</ul>
+</div>
